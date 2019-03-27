d4a914054f66e1109331b127f2ace95d38bd4724
Jason Beedle <jason_beedle@hotmail.com>
2019-03-27 07:22:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function prependKitten(name) {
   return [name, ...kittens];
 }
 function removeLastKitten(name) {
-  return kittens.slice(4);
+  return kittens.slice(2, kittens, length -1);
 }
 function removeFirstKitten(name){
     return kittens.slice(1)
